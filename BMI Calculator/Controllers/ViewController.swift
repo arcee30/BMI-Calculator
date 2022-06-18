5c14af3657debef7d79adbd8aa400964fdae72ff
arshaansayed <arshaan.h.sayed@gmail.com>
2022-06-18 19:14:14 -0400

    Initial Commit

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+  
+    
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var height: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+   
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightChanged(_ sender: UISlider) {
+        
+        self.height.text! = "\(Int(sender.value))in"
+    }
+    
+    
+    @IBAction func weightChanged(_ sender: UISlider) {
+        
+        self.weight.text! = "\(String(Int(sender.value)))Ib"
+    }
+    
+    
+    @IBAction func calc(_ sender: UIButton) {
+        let weight = weightSlider.value
+        let height = heightSlider.value
+        
+        print(height)
+        print(weight)
+        print( Float(weight * 703) / Float(pow(height,2)))
+    }
+    
 }
 
---
+++ BMI Calculator/Controllers/secondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  secondViewController.swift
+//  BMI Calculator
+//
+//  Created by Arshaan Sayed on 6/18/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+
+import UIKit
+class secondViewController : UIViewController {
+    
+}
